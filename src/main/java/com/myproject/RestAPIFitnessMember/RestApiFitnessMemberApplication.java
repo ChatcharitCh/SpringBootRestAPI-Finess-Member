46fcc3c08fc842c5a15d038d0fe40366e876c662
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-06-22 15:24:56 +0700

    Initial Commit

---
+++ src/main/java/com/myproject/RestAPIFitnessMember/RestApiFitnessMemberApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.RestAPIFitnessMember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiFitnessMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiFitnessMemberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/RestAPIFitnessMember/RestApiFitnessMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.RestAPIFitnessMember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiFitnessMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
