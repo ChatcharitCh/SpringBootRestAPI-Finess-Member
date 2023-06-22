6868a3a7f2185982c79ea285d8152737a7f6661a
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-06-22 15:33:50 +0700

    Created Model

---
+++ src/main/java/com/myproject/RestAPIFitnessMember/model/Customer.java
@@ -0,0 +1,138 @@
+
+package com.myproject.RestAPIFitnessMember.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ *
+ * @author Chatcharit
+ */
+@Entity
+@Table(name = "customers")
+public class Customer {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Enum gender;
+    private String email;
+    private String phone;
+    private Date birthDate;
+    private String address;
+    private float weight;
+    private float height;
+    private LocalDateTime registrationDate;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String firstName, String lastName, Enum gender, String email, String phone, Date birthDate, String address, float weight, float height, LocalDateTime registrationDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.weight = weight;
+        this.height = height;
+        this.registrationDate = registrationDate;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Enum getGender() {
+        return gender;
+    }
+
+    public void setGender(Enum gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+    
+}
